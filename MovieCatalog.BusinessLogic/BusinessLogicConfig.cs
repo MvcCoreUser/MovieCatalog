85b545fd6e9d711d93b02fa689ee34d8bfda2c13
Evgeniy Perekhodko <perekhodkoea@gmail.com>
2020-11-23 15:45:30 +0300

    configure services

---
+++ MovieCatalog.BusinessLogic/BusinessLogicConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MovieCatalog.DataAccess;
+
+namespace MovieCatalog.BusinessLogic
+{
+    public class BusinessLogicConfig
+    {
+        public static void Configure(IServiceCollection services, IConfiguration configuration)
+        {
+            DataConfig.Configure(services, configuration);
+        }
+    }
+}
--- MovieCatalog.Web/Startup.cs
+++ MovieCatalog.Web/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MovieCatalog.BusinessLogic;
 
 namespace MovieCatalog.Web
 {
@@ -24,6 +25,7 @@ namespace MovieCatalog.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            BusinessLogicConfig.Configure(services, Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
