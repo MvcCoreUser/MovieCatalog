751929afb5a00c84a9054f66085a4f08f41ec7cf
Evgeniy Perekhodko <perekhodkoea@gmail.com>
2020-11-23 15:49:29 +0300

    fix configurations

--- MovieCatalog.BusinessLogic/BusinessLogicConfig.cs
+++ MovieCatalog.BusinessLogic/BusinessLogicConfig.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MovieCatalog.BusinessLogic.Interfaces;
+using MovieCatalog.BusinessLogic.Services;
 using MovieCatalog.DataAccess;
 
 namespace MovieCatalog.BusinessLogic
@@ -13,7 +15,10 @@ namespace MovieCatalog.BusinessLogic
     {
         public static void Configure(IServiceCollection services, IConfiguration configuration)
         {
+
             DataConfig.Configure(services, configuration);
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IMovieService, MovieService>();
         }
     }
 }
